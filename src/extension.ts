22aee6a4655f4df593d2282112abb0df98e4476c
peter-mwau <kyalepeter2000@gmail.com>
2026-01-30 13:57:46 +0300

    created a panel view

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,48 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('codebasegps.helloKenya', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello Kenya from codebaseGPS!! Yes Buddy!');
+		// vscode.window.showInformationMessage('Hello Kenya from codebaseGPS!! Yes Buddy!');
+
+		const panel = vscode.window.createWebviewPanel(
+			'codebasegps', // Identifies the type of the webview. Used internally
+			'codebaseGPS', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{
+				enableScripts: true
+			} // Webview options. More on these later.
+		);
+
+		// And set its HTML content
+		panel.webview.html = getWebviewContent();
+
+		// Add this line to send a message to the webview:
+		panel.webview.postMessage({ command: 'alert' });
+
+		function getWebviewContent() {
+			return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>codebaseGPS</title>
+				<script>
+					const vscode = acquireVsCodeApi();
+					window.addEventListener('message', event => {
+						const message = event.data; // The JSON data our extension sent
+						switch (message.command) {
+							case 'alert':
+								document.getElementById('tag1').style.color = 'yellow';
+								break;
+						}
+					});
+				</script>
+			</head>
+			<body>
+				<h1>Hello Kenya from codebaseGPS!! Yes Buddy!</h1>
+				<p id="tag1">Welcome to your VS Code extension powered by Webview.</p>
+			</body>
+			</html>`;
+		}
 	});
 
 	context.subscriptions.push(disposable);
